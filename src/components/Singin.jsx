6aca10aaec9e65bbc8b2e4f50746ec952a1ec71b
Diyorbek <bekmurodovdiyorbek288@gmail.com>
2024-10-02 23:20:23 +0500

    salom

--- src/components/Singin.jsx
+++ src/components/Singin.jsx
@@ -1,103 +1,135 @@
+import React, { useState } from "react";
 import {
-    signInWithEmailAndPassword,
-    sendPasswordResetEmail,
-    signInWithPopup,
-    getAuth,
-    fetchSignInMethodsForEmail,
-  }
-   from "firebase/auth";
-   import Button from '@mui/material/Button';
-   import Stack from '@mui/material/Stack';
-  import { useState } from "react";
-  import { auth as firebaseAuth, provider } from "./Audifikatsiya";
-  import Hom from "./Home/Hom";
-  import google from "../assets/20221203181232!Google__G__logo.svg";
-  import "../assets/style/Singin.scss";
-  
-  const Singin = () => {
-    const [userEmail, setUserEmail] = useState("");
-    const [inputEmail, setInputEmail] = useState("");
-    const [password, setPassword] = useState("");
-  
-    const loginWithGoogle = () => {
-      signInWithPopup(firebaseAuth, provider)
-        .then((data) => {
-          setUserEmail(data.user.email);
-        })
-        .catch((error) => {
-          console.error("Google bilan tizimga kirishda xato:", error);
-        });
-    };
-  
-    const handleLogin = async () => {
-      if (inputEmail && password) {
-        const auth = getAuth();
-        try {
-          await signInWithEmailAndPassword(auth, inputEmail, password);
-          setUserEmail(inputEmail);
-        } catch (error) {
-          console.error("Login xatosi:", error);
-          alert("Loginda xato. Iltimos, email va parolni tekshiring.");
-        }
-      } else {
-        alert("Iltimos, email va parolni kiriting.");
-      }
-    };
-  
-    const handlePasswordReset = async () => {
+  signInWithEmailAndPassword,
+  sendPasswordResetEmail,
+  signInWithPopup,
+  getAuth,
+} from "firebase/auth";
+import Button from "@mui/material/Button";
+import IconButton from "@mui/material/IconButton";
+import InputAdornment from "@mui/material/InputAdornment";
+import OutlinedInput from "@mui/material/OutlinedInput";
+import InputLabel from "@mui/material/InputLabel";
+import FormControl from "@mui/material/FormControl";
+import Visibility from "@mui/icons-material/Visibility";
+import VisibilityOff from "@mui/icons-material/VisibilityOff";
+import Stack from "@mui/material/Stack";
+import { auth as firebaseAuth, provider } from "./Audifikatsiya";
+import Hom from "./Home/Hom";
+import google from "../assets/20221203181232!Google__G__logo.svg";
+import "../assets/style/Singin.scss";
+
+const Singin = () => {
+  const [userEmail, setUserEmail] = useState("");
+  const [inputEmail, setInputEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [showPassword, setShowPassword] = useState(false); // Yangi state qo'shildi
+
+  const handleClickShowPassword = () => {
+    setShowPassword(!showPassword);
+  };
+
+  const loginWithGoogle = () => {
+    signInWithPopup(firebaseAuth, provider)
+      .then((data) => {
+        setUserEmail(data.user.email);
+      })
+      .catch((error) => {
+        console.error("Google bilan tizimga kirishda xato:", error);
+      });
+  };
+
+  const handleLogin = async () => {
+    if (inputEmail && password) {
       const auth = getAuth();
-      if (inputEmail) {
-        try {
-          await sendPasswordResetEmail(auth, inputEmail);
-          alert("Parolni tiklash xabari yuborildi!");
-        } catch (error) {
-          console.error("Parolni tiklashda xato:", error);
-          alert("Parolni tiklashda xato. Iltimos, emailni tekshiring.");
-        }
-      } else {
-        alert("Iltimos, emailni kiriting.");
+      try {
+        await signInWithEmailAndPassword(auth, inputEmail, password);
+        setUserEmail(inputEmail);
+      } catch (error) {
+        console.error("Login xatosi:", error);
+        alert("Loginda xato. Iltimos, email va parolni tekshiring.");
       }
-    };
-  
-    return (
-      <div className="container">
-        {!userEmail ? (
-          <div className="usersingin">
-            <input
-              autoComplete="on"
+    } else {
+      alert("Iltimos, email va parolni kiriting.");
+    }
+  };
+
+  const handlePasswordReset = async () => {
+    const auth = getAuth();
+    if (inputEmail) {
+      try {
+        await sendPasswordResetEmail(auth, inputEmail);
+        alert("Parolni tiklash xabari yuborildi!");
+      } catch (error) {
+        console.error("Parolni tiklashda xato:", error);
+        alert("Parolni tiklashda xato. Iltimos, emailni tekshiring.");
+      }
+    } else {
+      alert("Iltimos, emailni kiriting.");
+    }
+  };
+
+  return (
+    <div className="container">
+      {!userEmail ? (
+        <div className="usersingin">
+          {/* <input
+            autoComplete="on"
+            type="text"
+            placeholder="user email"
+            value={inputEmail}
+            onChange={(e) => setInputEmail(e.target.value)}
+          /> */}
+          <FormControl variant="filled">
+            <InputLabel htmlFor="filled-adornment-email">Email</InputLabel>
+            <OutlinedInput
               type="text"
-              placeholder="user email"
+              id="filled-adornment-email"
               value={inputEmail}
               onChange={(e) => setInputEmail(e.target.value)}
             />
-            <input
-              autoComplete="on"
-              type="password"
-              placeholder="user password"
+          </FormControl>
+
+          <FormControl variant="filled">
+            <InputLabel htmlFor="standard-adornment-password">
+              Password
+            </InputLabel>
+            <OutlinedInput
+              type={showPassword ? "text" : "password"}
               value={password}
               onChange={(e) => setPassword(e.target.value)}
+              endAdornment={
+                <InputAdornment position="end">
+                  <IconButton onClick={handleClickShowPassword}>
+                    {showPassword ? <VisibilityOff /> : <Visibility />}
+                  </IconButton>
+                </InputAdornment>
+              }
+              label="Password"
             />
-<Stack spacing={2} direction="row">
-      <Button className="button" variant="contained"
-      onClick={handleLogin}
-      >Kirish</Button>
-    </Stack>
-            {/* <button onClick={handleLogin}>Kirish</button>
-            <button onClick={loginWithGoogle}>
-              <img src={google} alt="Google logo" /> Google orqali kirish
-            </button> */}
-            <Button variant="outlined" onClick={loginWithGoogle}><img src={google} alt="Google logo" />  Google orqali kirish</Button>
+          </FormControl>
 
-            <button onClick={handlePasswordReset}>
-              Parolni tiklash
-            </button>
-          </div>
-        ) : (
-          <Hom userEmail={userEmail} />
-        )}
-      </div>
-    );
-  };
-  
-  export default Singin;
-  
+          <Stack spacing={2} direction="row">
+            <Button
+              className="button"
+              variant="contained"
+              onClick={handleLogin}
+            >
+              Kirish
+            </Button>
+          </Stack>
+          <Button variant="outlined" onClick={loginWithGoogle}>
+            <img src={google} alt="Google logo" /> Google orqali kirish
+          </Button>
+          <Button variant="outlined" onClick={handlePasswordReset}>
+            Parolni tiklash
+          </Button>
+        </div>
+      ) : (
+        <Hom userEmail={userEmail} />
+      )}
+    </div>
+  );
+};
+
+export default Singin;
